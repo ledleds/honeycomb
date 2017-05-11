b9cacc371af0cc02432a3c1e9208c12e6c2a9254
Vicky Ledsom <victorialedsom@gmail.com>
2017-05-11 09:54:21 +0100

    Add before block to discount spec

--- spec/discount_spec.rb
+++ spec/discount_spec.rb
@@ -2,7 +2,7 @@ require './models/discount'
 
 describe Discount do
   subject { Discount.new }
-  let(:order) { double :order, subtotal: 0, express_delivery_count: 0 }
+  let(:order) { double :order }
 
   context 'to begin' do
     it 'has a discount amount of zero' do
@@ -10,16 +10,19 @@ describe Discount do
     end
   end
 
+  before do
+    allow(order).to receive(:express_delivery_count).and_return(2)
+    allow(order).to receive(:subtotal).and_return(50)
+  end
+
   context 'with an express delivery count of 2' do
     it 'adds 10 to amount' do
-      allow(order).to receive(:express_delivery_count).and_return(2)
       expect(subject.delivery_discount(order.express_delivery_count)).to eq(10)
     end
   end
 
   context 'with a subtotal of 50' do
     it 'adds 5 to amount' do
-      allow(order).to receive(:subtotal).and_return(50)
       expect(subject.overall_cost_discount(order.subtotal)).to eq(5)
     end
   end
